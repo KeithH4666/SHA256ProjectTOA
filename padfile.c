ec71e680e3f9a3f5eeaacad747eb2bef1d647e3e
Keith Higgins <keithhigginsowns@gmail.com>
2019-03-26 21:25:10 +0000

    working on padding

--- padfile.c
+++ padfile.c
@@ -11,7 +11,10 @@ int main(int argc, char *argv[]){
 
     union msgblock M;
 
+    uint64_t nobits = 0;
+
     uint64_t nobytes;
+
 
     FILE* f;
 
@@ -19,17 +22,24 @@ int main(int argc, char *argv[]){
 
     while(!feof(f)){
         nobytes = fread(M.e, 1, 64, f);
+        printf("Read %2llu bytes\n", nobytes);
+        nobits = nobits + (nobytes * 8);
         if (nobytes < 56){
             printf("%Found a block with less than 55 bytes!\n");
-            M.e[nobytes] = 0x01;
+            M.e[nobytes] = 0x80;
             while (nobytes < 56){
                 nobytes = nobytes +1;
                 M.e[nobytes] = 0x00;
             }
+            M.s[7] = nobits;
         }
     }
 
     fclose(f);
+
+    for(int i = 0; i < 64; i++)
+        printf("%x ",M.e[i]);
+    printf("\n");
     
     return 0;
 }
