6daea05d55bf9123335b2cad737160174573ae5b
Keith Higgins <keithhigginsowns@gmail.com>
2019-03-26 20:41:30 +0000

    reading files in

--- padfile.c
+++ padfile.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 #include <stdint.h>
 
+union msgblock {
+    uint8_t e[64];
+    uint32_t t[16];
+    uint64_t s[8];
+};
+
 int main(int argc, char *argv[]){
+
+    union msgblock M;
+
+    uint64_t nobytes;
+
+    FILE* f;
+
+    f = fopen(argv[1], "r");
+
+    while(!feof(f)){
+        nobytes = fread(M.e, 1, 64, f);
+        printf("%11u\n",nobytes);
+    }
+
+    fclose(f);
     
+    return 0;
 }
